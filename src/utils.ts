6f92489fe1349d3c463203170ad6f5ef23512ecb
Stout Ni <stout.officejob@gmail.com>
2025-12-08 02:24:11 +0800

    perf: memory leak in disconnected components

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
-export const appendToMap = <K, V>(map: Map<K, V[]>, propName: K, value: V) => {
-  const items = map.get(propName);
-  if (!items) {
-    map.set(propName, [value]);
-  } else if (!items.includes(value)) {
-    items.push(value);
+export const appendToMap = <K, V extends Object>(map: Map<K, WeakRef<V>[]>, propName: K, value: V) => {
+  const refs = map.get(propName);
+  if (!refs) {
+    map.set(propName, []);
+  } else if (!refs.some((ref) => ref.deref() === value)) {
+    refs.push(new WeakRef(value));
   }
 };
 
